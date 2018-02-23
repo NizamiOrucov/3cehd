713adb9505de92c9eb063aec7f6687c8c6870943
unknown <nizamibo@code.edu.az>
2018-02-23 20:31:55 +0400

    her shey yaxsi olacaq atam

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Imtahan balinizi yazin:");
+                var a = Console.ReadLine();
+                var b = Convert.ToInt32(a);
+                if (b < 51)
+                {
+                    Console.WriteLine("Get deden gelsin");
+                }
+                else
+                {
+                    if (b < 61)
+                    {
+                        Console.WriteLine("Yaxwidi");
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
